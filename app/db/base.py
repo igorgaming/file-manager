dd27e8e0fb67d697ecb3e49311bfe0854c6956de
Igor Sorokin <gosha.sorokin.2002@mail.ru>
2024-11-23 21:48:36 +0300

    Add naming convention for constraints

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,4 @@
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
@@ -6,6 +7,15 @@ from app.conf import settings
 engine = create_async_engine(settings.DATABASE_URI)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata_obj = MetaData(naming_convention=convention)
+
 
 class Base(DeclarativeBase):
-    pass
+    metadata = metadata_obj
