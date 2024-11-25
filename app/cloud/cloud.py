ad4626172722fef427ff9105cf7437724f8df22b
Igor Sorokin <gosha.sorokin.2002@mail.ru>
2024-11-25 15:20:40 +0300

    Add docstrings

--- app/cloud/cloud.py
+++ app/cloud/cloud.py
@@ -13,14 +13,49 @@ class Cloud:
         return self._location
 
     async def has_folder(self, path: str) -> bool:
+        """
+        Check if folder with given path exists in cloud storage.
+
+        Args:
+            path (str): Path to folder.
+
+        Returns:
+            bool: Returns `True` if folder exists.
+        """
+
         response = await self._client.get("has_folder/", {"path": self.get_link(path)})
         return response.get("status") is True
 
     async def create_file(self, path: str, file: BinaryIO) -> None:
+        """
+        Create a file in cloud storage.
+
+        Args:
+            path (str): Path to file.
+            file (BinaryIO): File to upload.
+        """
+
         await self._client.post(f"upload/{self.get_link(path)}", data=file)
 
     async def delete_file(self, path: str) -> None:
+        """
+        Delete a file from cloud storage.
+
+        Args:
+            path (str): Path to the file to be deleted.
+        """
+
         await self._client.post(f"delete/{self.get_link(path)}")
 
     def get_link(self, path: str) -> str:
+        """
+        Generate a full link to a resource in the cloud storage.
+
+        Args:
+            path (str): Relative path to the resource.
+
+        Returns:
+            str: Full URL to the resource in the cloud storage.
+        """
+
         return f"{self.location}{path}"
--- app/cloud/cloud_client.py
+++ app/cloud/cloud_client.py
@@ -27,11 +27,33 @@ class CloudClient:
         await self._close_connection()
 
     async def get(self, url: str, params: Optional[dict] = None) -> dict:
+        """
+        Send an asynchronous GET request to the specified URL.
+
+        Args:
+            url (str): The URL.
+            params (Optional[dict]): Query parameters.
+
+        Returns:
+            dict: The JSON response from the server.
+        """
+
         with self._ensure_ok():
             async with self._connection.get(url, params=params) as response:
                 return await response.json()
 
     async def post(self, url: str, data: Any = None) -> dict:
+        """
+        Send an POST request to the specified URL.
+
+        Args:
+            url (str): The URL.
+            data (Any): Data.
+
+        Returns:
+            dict: The JSON response from the server.
+        """
+
         with self._ensure_ok():
             async with self._connection.post(url, data=data) as response:
                 return await response.json()
